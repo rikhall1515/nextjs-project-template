2b07e6244a0e95d4d8494231fa3914d90d340974
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2024-04-12 19:38:27 +0200

    feat(repo): converted the repo into a turborepo
    This change configurates next.js to run with turborepo. Second, it configures eslint to work with
    jest and storybook, as well as adding good rules to keep code consistent across the repository. It
    also adds some stylelint and postcss configuration.
    
    BREAKING CHANGE: CI scripts will not work. CI and Vercel need to be configured to run with the new
    code.

---
+++ next.constants.mjs
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ * This is used to verify if the current Website is running on a Development Environment
+ */
+export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
+
+/**
+ * This is used for telling Next.js if the Website is deployed on Vercel
+ *
+ * Can be used for conditionally enabling features that we know are Vercel only
+ *
+ * @see https://vercel.com/docs/concepts/projects/environment-variables/system-environment-variables#framework-environment-variables
+ */
+export const VERCEL_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV || undefined;
+
+/**
+ * This is used for any place that requires the full canonical URL path for website and its deployment
+ *
+ * This variable can either come from the Vercel Deployment as `NEXT_PUBLIC_VERCEL_URL` or from
+ * the `NEXT_PUBLIC_BASE_URL` Environment Variable that is manually defined by us if necessary.
+ * Otherwise it will fallback to the default URL.
+ *
+ * @see https://vercel.com/docs/concepts/projects/environment-variables/system-environment-variables#framework-environment-variables
+ */
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+  ? process.env.NEXT_PUBLIC_BASE_URL
+  : process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "https://nextjs-project-template-rosy.vercel.app/";
+
+/**
+ * The `localStorage` key to store the theme choice of `next-themes`
+ *
+ * This is what allows us to store user preference for theming
+ */
+export const THEME_STORAGE_KEY = "theme";
