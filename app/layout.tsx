4fe346eb082cd78558f32e54c4a217b4455af7be
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2024-04-18 20:09:05 +0200

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { VERCEL_ENV } from "@/next.constants.mjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {VERCEL_ENV && (
+          <>
+            <Analytics />
+            <SpeedInsights />
+          </>
+        )}
+      </body>
     </html>
   );
 }
