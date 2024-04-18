d5bae6f999acdc0b0339eaf9e919c5bf22cf506d
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2024-04-18 19:53:27 +0200

    feat(env): add server t3 env validation
    Currently validates NODE_ENV

---
+++ env/server.ts
@@ -0,0 +1,19 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  isServer: typeof window === "undefined",
+  server: {
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+  },
+  /**
+   * Makes it so that empty strings are treated as undefined.
+   * `SOME_VAR: z.string()` and `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
+});
