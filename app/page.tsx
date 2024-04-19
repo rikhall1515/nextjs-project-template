17d7032d643e839ab5ec77792988b8b166e906ff
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2024-04-19 17:40:23 +0200

    feat: add metadata to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,65 @@
+import type { Metadata } from "next";
 import Image from "next/image";
+
+import { env } from "@/env/client";
+
+export const metadata: Metadata = {
+  title: "NextJS Template",
+  alternates: {
+    canonical: "/",
+  },
+  icons: {
+    apple: "/static/favicon/apple-touch-icon.png",
+    other: [
+      {
+        rel: "alternate icon",
+        sizes: "48x48",
+        url: "/static/favicon/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        sizes: "any",
+        url: "/static/favicon/favicon.svg",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/static/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/static/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "mask-icon",
+        color: "#0e0813",
+        url: "/public/static/favicon/safari-pinned-tab.svg",
+      },
+    ],
+  },
+  openGraph: {
+    title: "NextJS Template - rikhall.proj",
+    description:
+      "This is a Next.JS template designed and made by Rikard Hallberg.",
+    url: env.NEXT_PUBLIC_BASE_URL,
+    images: "/static/og.png",
+    siteName: "RikardTemplate",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    title: "NextJS Template - rikhall.proj",
+    description:
+      "This is a Next.JS template designed and made by Rikard Hallberg.",
+    images: "/static/og.png",
+    card: "summary_large_image",
+    creator: "@rikhall1515",
+  },
+};
 
 export default function Home() {
   return (
