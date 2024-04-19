b91c2acf1f281ae1ba66f6bea49d68750ebfe27f
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2024-04-19 18:24:00 +0200

    feat: add metadata to sign in page

--- app/(public)/sign-in/page.tsx
+++ app/(public)/sign-in/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -6,7 +7,63 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { createClient } from "@/db/server";
+import { env } from "@/env/client";
 
+export const metadata: Metadata = {
+  title: "Sign In",
+  alternates: {
+    canonical: "/sign-in",
+  },
+  icons: {
+    apple: "/static/favicon/apple-touch-icon.png",
+    other: [
+      {
+        rel: "alternate icon",
+        sizes: "48x48",
+        url: "/static/favicon/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        sizes: "any",
+        url: "/static/favicon/favicon.svg",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/static/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/static/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "mask-icon",
+        color: "#0e0813",
+        url: "/public/static/favicon/safari-pinned-tab.svg",
+      },
+    ],
+  },
+  openGraph: {
+    title: "Sign In - rikhall.proj",
+    description: "The sign in page for the NextJS template.",
+    url: env.NEXT_PUBLIC_BASE_URL,
+    images: "/next-data/og",
+    siteName: "RikardTemplate",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    title: "Sign In - rikhall.proj",
+    description: "The sign in page for the NextJS template.",
+    images: "/next-data/og",
+    card: "summary_large_image",
+    creator: "@rikhall_",
+  },
+};
 export default function SignIn() {
   const signIn = async (formData: FormData) => {
     "use server";
