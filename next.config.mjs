704c721812beb2098bfa56d7dd4f93099cc8be23
Rikard Hallberg <r15.hallberg@gmail.com>
2024-04-20 23:08:48 +0200

    fix(env): add jiti build time validation (#40)
    * fix(env): add jiti build time validation
    
    I think this addresses the open issue on why env validation did not work.
    
    fix #37
    
    * fix(github): add env variables to build action
    
    * chore: test env variables in different location
    
    * chore: test with secrets for env
    
    * chore: test with cross-env
    
    * chore: add env to both local and global scope
    
    * chore: add workflow dispatch to build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 "use strict";
+import { fileURLToPath } from "node:url";
+
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
 
 import { BASE_PATH } from "./next.constants.mjs";
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./env/client.ts");
+jiti("./env/server.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
