61372c6cd5cecc01fc0a681eea9da14d056fab93
rikhall1515 <r15.hallberg@gmail.com>
2024-04-21 21:25:09 +0200

    feat: add cookie component

---
+++ components/cookies/index.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { CookieConsent } from "react-cookie-consent";
+
+import { useCookieContext } from "@/context/cookie";
+
+export default function CookieBanner() {
+  const consent = useCookieContext();
+
+  return consent.hasConsentValue ? (
+    <></>
+  ) : (
+    <>
+      <CookieConsent
+        enableDeclineButton
+        sameSite="strict"
+        flipButtons
+        cookieName="giftistguide_consent"
+        buttonText="Accept all"
+        declineButtonText="Decline all"
+        onAccept={() => {
+          consent.setHasConsentValue(true);
+        }}
+        onDecline={() => {
+          consent.setHasConsentValue(true);
+        }}
+        style={{
+          background: "hsl(var(--popover))",
+          color: "hsl(var(--popover-foreground))",
+          borderTop: "1px hsl(var(--border))",
+        }}
+        buttonStyle={{
+          background: "hsl(var(--primary))",
+          color: "white",
+          borderRadius: "0.35rem",
+        }}
+        declineButtonStyle={{
+          border: "1px hsl(var(--ring))",
+          background: "hsl(var(--secondary))",
+          color: "hsl(var(--secondary-foreground))",
+        }}
+        buttonClasses=""
+        declineButtonClasses=""
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
+    </>
+  );
+}
