fe607074ce8b842e9160fbe711c99b7f6af259a1
rikhall1515 <r15.hallberg@gmail.com>
2024-04-21 22:15:42 +0200

    chore: add tailwind screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      screens: {
+        m: "420px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
