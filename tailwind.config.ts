5d6d9f7642f66048a4b35a9362eb76d603cb0a39
rikhall1515 <r15.hallberg@gmail.com>
2024-04-22 22:58:07 +0200

    chore(tailwind): add aceternity ui code for grids

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,27 @@
+import svgToTinyDataUri from "mini-svg-data-uri";
 import type { Config } from "tailwindcss";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+// type TRecord = Record<string, string> | string;
+// function addVariablesForColors({
+//   addBase,
+//   theme,
+// }: {
+//   addBase: (arg0: Record<string, TRecord>) => void;
+//   theme: (arg0: string) => Record<string, string>;
+// }) {
+//   const allColors = flattenColorPalette(theme("colors"));
+//   const newVars = Object.fromEntries(
+//     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+//   );
+
+//   addBase({
+//     ":root": newVars,
+//   });
+// }
+
+/** @type {import('tailwindcss').Config} */
 const config = {
   darkMode: ["class"],
   content: [
@@ -70,10 +92,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        gradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "100%": { backgroundPosition: "150% 50%" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        gradient: "gradient 6s linear infinite",
       },
     },
   },
@@ -81,6 +108,28 @@ const config = {
     require("tailwindcss-animate"),
     require("@savvywombat/tailwindcss-grid-areas"),
     require("@tailwindcss/container-queries"),
+    function ({ matchUtilities, theme }: unknown) {
+      matchUtilities(
+        {
+          "bg-grid": (value: string) => ({
+            backgroundImage: `url("${svgToTinyDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: string) => ({
+            backgroundImage: `url("${svgToTinyDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: string) => ({
+            backgroundImage: `url("${svgToTinyDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
   ],
 } satisfies Config;
 
