3a2ca619312c8b6d414b956324b2e239df1a098c
rikhall1515 <r15.hallberg@gmail.com>
2024-04-23 19:06:35 +0200

    fix(skipbtn): make the link focus visible and add id to main

--- components/header/util/jumpToContent.tsx
+++ components/header/util/jumpToContent.tsx
@@ -8,7 +8,7 @@ export default function JumpToContent() {
         asChild
         className={cn(
           "absolute left-0 top-0 z-50 block -translate-y-[200%] rounded-none rounded-br-md transition-all",
-          "focus-visible:translate-y-0"
+          "focus:translate-y-0 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 focus-visible:translate-y-0"
         )}
       >
         <a href="#content">Skip to Content</a>
