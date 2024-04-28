33ef4a3ee1a2369b343730c05a678bf9d0ff7716
rikhall1515 <r15.hallberg@gmail.com>
2024-04-29 00:32:53 +0200

    feat: add dashboard context with localstorage support

---
+++ context/dashboard.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import {
+  type Dispatch,
+  type SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type DashboardStore = {
+  isExpanded: boolean;
+  setIsExpanded: Dispatch<SetStateAction<boolean>>;
+  toggle: () => void;
+};
+
+export const DashboardContext = createContext<DashboardStore | undefined>(
+  undefined
+);
+export function useDashboardContext() {
+  const sidebarStore = useContext(DashboardContext);
+
+  if (sidebarStore === undefined) {
+    throw new Error(
+      "useDashboardContext must be used with a DashboardContext provider"
+    );
+  }
+
+  return sidebarStore;
+}
+
+export default function DashboardContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isExpanded, setIsExpanded] = useState(true);
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    setIsExpanded(
+      localStorage.getItem("preference_sidebar_expanded") === "true"
+    );
+  }, []);
+  const toggle = () => {
+    setIsExpanded(!isExpanded);
+    localStorage.setItem("preference_sidebar_expanded", `${!isExpanded}`);
+  };
+  return (
+    <>
+      <DashboardContext.Provider
+        value={{
+          isExpanded,
+          setIsExpanded,
+          toggle,
+        }}
+      >
+        {children}
+      </DashboardContext.Provider>
+    </>
+  );
+}
