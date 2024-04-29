fe35b7eea8f911c928182b22879b525698ba93b0
rikhall1515 <r15.hallberg@gmail.com>
2024-04-29 23:04:08 +0200

    feat: add demo privacy policy

---
+++ app/(public)/legal/privacy-policy/page.tsx
@@ -0,0 +1,87 @@
+import Wrapper from "./wrapper";
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <Wrapper>
+        <h1 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          Privacy Policy
+        </h1>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          rikhall.proj (below called &quot;We&quot; or &quot;Us&quot;) is
+          committed to protecting your data integrity. This privacy policy
+          describes how we collect, use, and protect your personal data when you
+          use our website.
+        </p>
+        <h2 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          1. Collected data
+        </h2>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          We currently only store data related to you logging in and out of this
+          website, meaning passwords (if any), and emails. We do not store any
+          more data unless you freely give that data to us with your consent.
+        </p>
+        <h2 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          2. Usage of data
+        </h2>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          The data we collect would be for the sake of making the website
+          function. We currently do not store any other kinds of data that are
+          not strictly for the sake of functionality.
+        </p>
+        <h2 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          3. Sharing of data
+        </h2>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          We do not share your personal information with third parties without
+          your consent, unless required by law.
+        </p>
+        <h2 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          4. Storage of data
+        </h2>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          We store your personal data securely and will erase the data when they
+          are either no longer necessary for the original intent of the
+          collection. If you request your data to be deleted, we will do so.
+        </p>
+        <h2 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          5. Cookies
+        </h2>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          We use cookies on this website to improve your user experience and
+          make it function. Some of these cookies are strictly necessary. This
+          includes cookies for authentication. We allow you to withdraw consent
+          from all non-essential cookies at any time in the footer section of
+          this website. You can press &quot;Cookie Settings&quot; to learn more.
+          <br />
+          <br />
+          Another option is that you configure your browser to block said
+          cookies or remove them, if you want that.
+        </p>
+        <h2 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          6. Your rights to your data
+        </h2>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          You have the right to request your data or to correct parts of it or
+          its entirety. You have the right to recall your consent at any time.
+        </p>
+        <h2 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          7. Changes to our privacy policy
+        </h2>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          This Privacy Policy will change at certain points in time in the
+          future. These changes will be published on our website.
+        </p>
+        <h2 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          8. Contact us
+        </h2>
+        <p className="mb-[3rem] max-w-[60ch] text-[1.125rem] leading-[1.4] m:text-[1.25rem]">
+          If you have any questions regarding the privacy policy or how your
+          data is treated, please contact us at r15.hallberg@gmail.com <br />{" "}
+          <br />
+          Last changed: 2024-04-29
+        </p>
+      </Wrapper>
+    </>
+  );
+}
