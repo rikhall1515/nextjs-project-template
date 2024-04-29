060b3a660abeb1a7f9707beb646eca577d1136f7
rikhall1515 <r15.hallberg@gmail.com>
2024-04-29 23:02:45 +0200

    feat: add empty company details page

---
+++ app/(public)/legal/company-details/page.tsx
@@ -0,0 +1,13 @@
+import Wrapper from "./wrapper";
+
+export default function Company() {
+  return (
+    <>
+      <Wrapper>
+        <h1 className="mb-[1.5rem] text-[1.5rem] font-bold leading-[1.3] m:text-[2.5rem]">
+          Company details
+        </h1>
+      </Wrapper>
+    </>
+  );
+}
