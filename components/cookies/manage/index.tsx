1e6ec74a0420924edfb7a45d5067477ead11a093
rikhall1515 <r15.hallberg@gmail.com>
2024-05-04 00:35:58 +0200

    feat: assemble manage consent menu

---
+++ components/cookies/manage/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+import TabNav from "@/components/cookies/manage/tabNav";
+import { useCookieContext } from "@/context/cookie";
+import { cn } from "@/lib/utils";
+
+export default function ManageConsent() {
+  const cookieStore = useCookieContext();
+  return (
+    <>
+      <div
+        className={cn(
+          "cover bottom-[-100%] left-[-100%] right-[-100%] top-[-100%] m-auto h-[100vh] w-full bg-opacity-50 !bg-no-repeat object-cover backdrop-blur backdrop-brightness-50",
+          "fixed z-40 overflow-hidden bg-black bg-opacity-50",
+          cookieStore.isManageConsentMenuOpen ? "block" : "hidden"
+        )}
+        id="#ManageConsentMenuDialog"
+      >
+        <div
+          className={cn(
+            " flex flex-col items-center rounded-2xl text-foreground",
+            "fixed left-1/2 top-1/2 z-[99] box-border -translate-x-1/2 -translate-y-1/2 overflow-hidden",
+            "h-auto max-h-[calc(100%-16px)] w-[calc(100%-16px)] max-w-[80rem] rounded-2xl bg-background pt-6 shadow transition-all"
+          )}
+        >
+          <TabNav />
+        </div>
+      </div>
+    </>
+  );
+}
