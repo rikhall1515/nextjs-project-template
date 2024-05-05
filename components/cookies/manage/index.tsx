d35a6b2b3934f778d5bb8dd729be2b160c9f79a9
rikhall1515 <r15.hallberg@gmail.com>
2024-05-05 23:24:20 +0200

    refactor: pass submit handlers into tabnav

--- components/cookies/manage/index.tsx
+++ components/cookies/manage/index.tsx
@@ -1,17 +1,19 @@
 "use client";
 import TabNav from "@/components/cookies/manage/tabNav";
 import { useCookieContext } from "@/context/cookie";
+import { useCookieFormContext } from "@/context/cookie/form";
 import { cn } from "@/lib/utils";
 
-export default function ManageConsent() {
-  const cookieStore = useCookieContext();
+export default function ManageWrapper() {
+  const cookie = useCookieContext();
+  const { handleSubmit, onSubmit } = useCookieFormContext();
   return (
     <>
       <div
         className={cn(
           "cover bottom-[-100%] left-[-100%] right-[-100%] top-[-100%] m-auto h-[100vh] w-full bg-opacity-50 !bg-no-repeat object-cover backdrop-blur backdrop-brightness-50",
           "fixed z-40 overflow-hidden bg-black bg-opacity-50",
-          cookieStore.isManageConsentMenuOpen ? "block" : "hidden"
+          cookie.isManageConsentMenuOpen ? "block" : "hidden"
         )}
         id="#ManageConsentMenuDialog"
       >
@@ -22,7 +24,7 @@ export default function ManageConsent() {
             "h-auto max-h-[calc(100%-16px)] w-[calc(100%-16px)] max-w-[80rem] rounded-2xl bg-background pt-6 shadow transition-all"
           )}
         >
-          <TabNav />
+          <TabNav handleSubmit={handleSubmit} onSubmit={onSubmit} />
         </div>
       </div>
     </>
