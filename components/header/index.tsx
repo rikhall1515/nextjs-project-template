a99e1bae91078251f65a7be6c8ba82f3ec77b4cb
rikhall1515 <r15.hallberg@gmail.com>
2024-05-08 13:39:22 +0200

    perf(header): remove unnecessary stylistic mounting logic
    Header looks perfectly fine without the animation.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,18 +1,27 @@
+import BaseLogo from "@/components/logo";
 import SidebarContextProvider from "@/context/sidebar";
+import { env } from "@/env/client";
 import { cn } from "@/lib/utils";
 
-import Logo from "./logo";
 import Nav from "./menu";
 import Sidebar from "./sidebar";
 import JumpToContent from "./util/jumpToContent";
-import HeaderWrapper from "./util/wrapper";
 
 export default function Header() {
   return (
     <>
       <JumpToContent />
       <SidebarContextProvider>
-        <HeaderWrapper>
+        <header
+          className={cn(
+            "fixed left-0 right-0 top-0 z-10", //position
+            "h-[8rem] w-[100svw]", //size
+            "border-b-[1px] border-[transparent] bg-[initial]", //color
+            "transition-[height,border,background-color] duration-300", //transition
+            "h-[4.5rem] border-b border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60",
+            "backdrop-blur"
+          )}
+        >
           <div
             className={cn(
               "flex items-center justify-between",
@@ -21,12 +30,24 @@ export default function Header() {
             )}
           >
             <div className="flex items-center gap-12">
-              <Logo />
+              <a
+                href="/"
+                className={cn(
+                  "h-8",
+                  "flex items-center gap-1",
+                  "text-base font-bold tracking-[0.2em]",
+                  "text-foreground"
+                )}
+              >
+                <BaseLogo />
+                <span className="text-3xl">/</span>
+                <span className="-mb-2">{env.NEXT_PUBLIC_SITE_NAME}</span>
+              </a>
               <Nav />
             </div>
             <Sidebar />
           </div>
-        </HeaderWrapper>
+        </header>
       </SidebarContextProvider>
     </>
   );
