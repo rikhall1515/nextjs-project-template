a38bb858e578476406b84a3a9a0d89adcd2a86c0
rikhall1515 <r15.hallberg@gmail.com>
2024-05-08 13:58:13 +0200

    refactor(sidebar): add sidebar to dialog, remove unneeded context
    This finalizes the change, and it is breaking with previous versions because previous context has
    been removed.
    
    BREAKING CHANGE: Previous context has been removed because it is no longer necessary.

--- components/header/sidebar/topbar/menuButton.tsx
+++ components/header/sidebar/topbar/menuButton.tsx
@@ -1,25 +1,23 @@
 "use client";
-import { forwardRef } from "react";
-
 import { useHeaderAPIContext } from "@/context/sidebar";
 import { cn } from "@/lib/utils";
 
-type Props = JSX.IntrinsicElements["button"];
-
-type Ref = HTMLButtonElement;
-export const MenuButton = forwardRef<Ref, Props>(function MenuButton() {
+export default function InnerMenuButton() {
   const { toggleSidebarDialog } = useHeaderAPIContext();
   return (
     <>
       <button
-        aria-expanded={false}
-        aria-label={"open menu"}
+        aria-expanded={true}
+        aria-label={"Close menu"}
         onClick={toggleSidebarDialog}
-        className={cn("relative z-[30] fill-[inherit] p-6")}
+        className={cn("relative z-[30] mr-6 fill-[inherit] p-6")}
+        onKeyDown={(e) => {
+          if (e.code === "Escape") toggleSidebarDialog();
+        }}
         aria-haspopup="menu"
         aria-controls="sidebarMenuNav"
       >
-        <div className={cn("menuBtn h-6 w-6")}>
+        <div className={cn("menuBtn opened h-6 w-6")}>
           <span></span>
           <span></span>
           <span></span>
@@ -27,4 +25,4 @@ export const MenuButton = forwardRef<Ref, Props>(function MenuButton() {
       </button>
     </>
   );
-});
+}
